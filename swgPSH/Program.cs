209568f131e8b19db1585e0c91b33dfd49f22b5d
kurdtkobain <dbz81487@yahoo.com>
2015-10-08 10:41:42 -0400

    Initial Commit

---
+++ swgPSH/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swgPSH
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] FORM = new byte[] { 0x46, 0x4F, 0x52, 0x4D };
+                byte[] PSHFORM = new byte[] { 0x50, 0x53, 0x48, 0x50, 0x46, 0x4F, 0x52, 0x4D };
+                byte[] PSRC = new byte[] { 0x30, 0x30, 0x30, 0x30, 0x50, 0x53, 0x52, 0x43 };
+                byte[] PEXE = new byte[] { 0x50, 0x45, 0x58, 0x45 };
+                using (BinaryWriter bw = new BinaryWriter(ms))
+                {
+                    bw.BaseStream.Position = 0x14;
+                    bw.Write(PSRC);
+                    BinaryReader br = new BinaryReader(File.OpenRead(args[0] + ".hlsl"));
+                    byte[] bytespsrc = BitConverter.GetBytes((int)br.BaseStream.Length + 1);
+                    Array.Reverse(bytespsrc);
+                    bw.Write(bytespsrc);
+                    bw.Write(br.ReadBytes((int)br.BaseStream.Length));
+                    bw.Write((byte)0x00);
+                    br.Close();
+                    bw.Write(PEXE);
+                    br = new BinaryReader(File.OpenRead(args[0] + ".hlsl.comp"));
+                    byte[] bytespexe = BitConverter.GetBytes((int)br.BaseStream.Length);
+                    Array.Reverse(bytespexe);
+                    bw.Write(bytespexe);
+                    bw.Write(br.ReadBytes((int)br.BaseStream.Length));
+                    br.Close();
+                    int tmp = (int)bw.BaseStream.Length - 0x14;
+                    bw.BaseStream.Position = 0x08;
+                    bw.Write(PSHFORM);
+                    byte[] bytespshform = BitConverter.GetBytes(tmp);
+                    Array.Reverse(bytespshform);
+                    bw.Write(bytespshform);
+                    byte[] bytesform = BitConverter.GetBytes((int)bw.BaseStream.Length - 0x08);
+                    Array.Reverse(bytesform);
+                    bw.BaseStream.Position = 0;
+                    bw.Write(FORM);
+                    bw.Write(bytesform);
+                }
+                BinaryWriter filebw = new BinaryWriter(File.Open(args[0] + ".psh", FileMode.Create));
+                filebw.Write(ms.ToArray());
+                filebw.Close();
+            }
+
+
+        }
+    }
+}
