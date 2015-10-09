d1a4deddbd0b302285847f2bba9b7da6143c26d1
kurdtkobain <dbz81487@yahoo.com>
2015-10-09 11:29:14 -0400

    tools for dumping psh files

---
+++ swgHLSLStrip/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace swgHLSLStrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists(args[0]))
+            {
+                var SLines = File.ReadAllLines(args[0]);
+                if (SLines[0] == "ps.1.1" || SLines[0] == "ps.1.4" || SLines[0] == "ps.2.0")
+                {
+                    if (SLines[0] == "ps.1.1")
+                    {
+                        SLines[0] = "ps_1_1";
+                    }
+                    if (SLines[0] == "ps.1.4")
+                    {
+                        SLines[0] = "ps_1_4";
+                    }
+                    if (SLines[0] == "ps.2.0")
+                    {
+                        SLines[0] = "ps_2_0";
+                    }
+                    Console.WriteLine(SLines[0]);
+                    File.WriteAllLines(args[0], SLines);
+                }
+            }
+        }
+    }
+}
---
+++ swgPSHDump/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace swgPSHDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists(args[0]))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    BinaryReader br = new BinaryReader(File.OpenRead(args[0]));
+                    br.ReadBytes(4);
+                    int formL = br.ReadInt32();
+                    br.ReadBytes(8);
+                    int formPSHL = br.ReadInt32();
+                    br.ReadBytes(8);
+                    byte[] tmp = br.ReadBytes(4);
+                    Array.Reverse(tmp);
+                    int pshSRCL = BitConverter.ToInt32(tmp,0);
+                    Console.WriteLine("SRC size:" + pshSRCL.ToString());
+                    using (BinaryWriter bw = new BinaryWriter(ms))
+                    {
+                        bw.Write(br.ReadBytes(pshSRCL-1));
+                    }
+                    br.Close();
+                    using (BinaryWriter bw = new BinaryWriter(File.Open(args[1], FileMode.Create)))
+                    {
+                        bw.Write(ms.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}
